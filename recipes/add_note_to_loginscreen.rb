d277152e87054e86f017c715909475ca5d8ecd80
Roderik van der Veer <roderik@vanderveer.be>
2013-02-19 12:39:56 +0100

    Add the add_note_to_loginscreen recipe

--- recipes/add_note_to_loginscreen.rb
+++ recipes/add_note_to_loginscreen.rb
@@ -1,5 +1,4 @@
-osxdefaults_defaults "Add a note to the loginscreen" do
-  domain 'com.apple.loginwindow.plist'
-  key 'LoginwindowText'
-  string node['osxdefaults']['loginmessage']
+# Do this by hand because the provider does not handle spaces in the value well
+execute "Add a note to the loginscreen - com.apple.loginwindow.plist - LoginwindowText"  do
+    command "defaults write \"com.apple.loginwindow.plist\" \"LoginwindowText\" -string \"#{node['osxdefaults']['loginmessage']}\""
 end
