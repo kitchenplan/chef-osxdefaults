8e74f55a3afb97ec128a4638faf1423c08c614af
Roderik van der Veer <roderik@vanderveer.be>
2013-02-23 10:17:33 +0100

    Rename and sort

---
+++ recipes/dock_minimize_to_application.rb
@@ -0,0 +1,5 @@
+osxdefaults_defaults "Dock: Minimize to application" do
+  domain 'com.apple.dock'
+  key "minimize-to-application"
+  integer 1
+end
---
+++ recipes/dock_set_magnification_on.rb
@@ -0,0 +1,11 @@
+osxdefaults_defaults "Dock: Set magnification on" do
+  domain 'com.apple.dock'
+  key 'magnification'
+  integer 1
+end
+
+osxdefaults_defaults "Dock: Set magnification level" do
+  domain 'com.apple.dock'
+  key 'magnification'
+  float 70.0
+end
