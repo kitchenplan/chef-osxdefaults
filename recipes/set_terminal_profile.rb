15f2c7629ba5f49ce96b9192aed68445de760229
Roderik van der Veer <roderik@vanderveer.be>
2013-02-24 09:24:08 +0100

    Set the custom TomorrowNightEighties terminal theme

--- recipes/set_terminal_profile.rb
+++ recipes/set_terminal_profile.rb
@@ -1,11 +1,27 @@
+template "#{Chef::Config[:file_cache_path]}/TomorrowNightEighties.terminal" do
+    source "TomorrowNightEighties.terminal"
+    owner node['current_user']
+    mode "0755"
+end
+
+execute "Load the terminal theme" do
+    command "open #{Chef::Config[:file_cache_path]}/TomorrowNightEighties.terminal"
+end
+
+execute "Wait a while" do
+    command "sleep 5"
+end
+
 osxdefaults_defaults "Default Window Settings" do
   domain 'com.apple.Terminal'
   key 'Default Window Settings'
-  string 'Pro'
+  string 'TomorrowNightEighties'
 end
 
 osxdefaults_defaults "Startup Window Settings" do
   domain 'com.apple.Terminal'
   key 'Startup Window Settings'
-  string 'Pro'
+  string 'TomorrowNightEighties'
 end
+
+
