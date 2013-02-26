3faa3c16305af398f123746e19df3cb5cd57897d
Roderik van der Veer <roderik@vanderveer.be>
2013-02-26 17:13:14 +0100

    Some tweaks to the terminal settings and don't open the window if the theme is already set. This fixes https://github.com/kitchenplan/chef-osxdefaults/issues/1

--- recipes/set_terminal_profile.rb
+++ recipes/set_terminal_profile.rb
@@ -1,27 +1,33 @@
+
 template "#{Chef::Config[:file_cache_path]}/TomorrowNightEighties.terminal" do
     source "TomorrowNightEighties.terminal"
     owner node['current_user']
     mode "0755"
+    not_if "defaults read \"com.apple.Terminal\" \"Default Window Settings\" | grep ^TomorrowNightEighties$", :user => node['current_user']
 end
 
 execute "Load the terminal theme" do
     command "open #{Chef::Config[:file_cache_path]}/TomorrowNightEighties.terminal"
+    not_if "defaults read \"com.apple.Terminal\" \"Default Window Settings\" | grep ^TomorrowNightEighties$", :user => node['current_user']
 end
 
 execute "Wait a while" do
     command "sleep 5"
+    not_if "defaults read \"com.apple.Terminal\" \"Default Window Settings\" | grep ^TomorrowNightEighties$", :user => node['current_user']
 end
 
 osxdefaults_defaults "Default Window Settings" do
   domain 'com.apple.Terminal'
   key 'Default Window Settings'
   string 'TomorrowNightEighties'
+  not_if "defaults read \"com.apple.Terminal\" \"Default Window Settings\" | grep ^TomorrowNightEighties$", :user => node['current_user']
 end
 
 osxdefaults_defaults "Startup Window Settings" do
   domain 'com.apple.Terminal'
   key 'Startup Window Settings'
   string 'TomorrowNightEighties'
+  not_if "defaults read \"com.apple.Terminal\" \"Default Window Settings\" | grep ^TomorrowNightEighties$", :user => node['current_user']
 end
 
 
