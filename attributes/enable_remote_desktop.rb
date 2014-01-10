3129ec461e5ec918284a470081b64f5f4d3f38e6
Ryan Sonnek <ryan@codecrate.com>
2014-01-10 18:37:06 +0100

    remove garage specific config

--- attributes/enable_remote_desktop.rb
+++ attributes/enable_remote_desktop.rb
@@ -1,2 +1,2 @@
 node.default['osxdefaults']['remote_desktop_path'] = "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart"
-node.default['osxdefaults']['remote_desktop_config'] = "-activate -configure -access -on -restart -agent -privs -all -allowAccessFor -specifiedUsers -users garageadmin"
+node.default['osxdefaults']['remote_desktop_config'] = "-activate -configure -access -on -restart -agent -privs -all -allowAccessFor -allUsers"
